e019a657a7dbb1c762cb2c75b636996b4226904c
redaimid <Iredaimid@gmail.com>
2025-02-18 21:10:48 +0500

    Create Config.py

---
+++ Config.py
@@ -0,0 +1,8 @@
+# Токен доступа к VK API (бота)
+TOKEN = 'vk1.a.GS-0hKlS0YwAN3eGUr_A4_OjAUAhk1gXH7qZ8C43R7k4ux2o5EU0SK6wj7OvJ-zKGTgBjeldNbOfQm4xKItpkeewnEts4vepnybfIHEufRlMXPCLhLQLhXIeH-GyK0qpj4DVrrbbBDbIhpFDfAZiL8zS7qj9vXMWrIU_Tjl5HWYohGZKK4c10m0Gl242_qPpm8qr_Ssm0D-3aRm7aCjK_Q'
+# ID группы VK
+GROUP_ID = '229419604'                                         
+# URL Supabase
+SUPABASE_URL = 'https://pccjtryqkpezpnynsvhm.supabase.co'      
+# API-ключ Supabase
+SUPABASE_API_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBjY2p0cnlxa3BlenBueW5zdmhtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzk4MTAzNTQsImV4cCI6MjA1NTM4NjM1NH0._KqipB5qGw2q1NZFcsD6V2d8766jOwwGZ9dri6LApDw'
